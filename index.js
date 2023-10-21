c02a9119786b192bb33c278e43d4540ecc955660
Hala Qitouqa <h.qitouqa@gmail.com>
2023-10-21 17:44:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+
+ function submitData(name,email,id){
+    const configurationObject = {
+        method:'POST',
+        headers: {
+          'Content-Type': "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({name,email,id})
+            
+    };
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    document.body.innerHTML = object['id']
+  })
+  .catch(function (error) {
+    document.body.innerHTML = error.message
+  })
+}
